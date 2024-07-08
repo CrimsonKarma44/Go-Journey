3ab86f1cdc8b3526b5909884bad102832aeb6613
Vincent Princewill <vincentprincewill44@gmail.com>
2024-07-08 10:45:52 +0100

    Update (urlshortner)

--- UrlShortner/models/url.go
+++ UrlShortner/models/url.go
@@ -2,24 +2,30 @@ package models
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
 
 type Url struct {
+	gorm.Model
 	IS    int
 	Url   string
 	Alias string
-
-	created time.Time
-	Updated time.Time
 }
 
-func (u *Url) Created() time.Time {
-	return u.created
+func (u *Url) Created() string {
+	return string(u.CreatedAt.Day()) + string(u.CreatedAt.Hour()) + ":" + string(u.CreatedAt.Minute()) + ":" + string(u.CreatedAt.Second())
 }
-func (u *Url) Update(newAlias string) {
-	u.Alias = newAlias
-	u.Updated = time.Now()
-}
-func (u *Url) Save() *gorm.DB {
+
+//	func (u *Url) Update(newAlias string) {
+//		u.Alias = newAlias
+//		u.Updated = time.Now()
+//	}
+//func (u *Url) Save() *gorm.DB {
+//}
+
+var db *gorm.DB
+
+func RevealUrl() []Url {
+	var urls []Url
+	db.Find(&urls)
+	return urls
 }
