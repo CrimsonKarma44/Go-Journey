45cd968fed9d7fab0626227de2d644f8b95631d5
Vincent Princewill <vincentprincewill44@gmail.com>
2024-11-06 08:26:19 +0100

    6 nov

--- algorithms/main.go
+++ algorithms/main.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	// fmt.Println(sorting_algorithm.BubbleSort([]int{10, 4, 5, 6, 7}))
 	// fmt.Println(sorting_algorithm.SelectSort([]int{64, 25, 12, 22, 11}))
-	fmt.Println(sorting_algorithm.InsertionSort([]int{23, 1, 10, 5, 2}))
+	fmt.Println(sorting_algorithm.InsertionSort([]int{23, 1, 10, 5, 2, 4}))
 }
